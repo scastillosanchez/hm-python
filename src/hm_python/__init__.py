2f1b8fc3b6c7d861949c30691bb73a373f3cf3ce
Sebastian Castillo-Sanchez <scastillosanchez@Sebastians-MBP.home>
2020-06-05 21:01:07 -0400

    Add tutorial files

---
+++ src/hm_python/__init__.py
@@ -0,0 +1,2 @@
+# src/hm_python/__init__.py
+__version__ = "0.1.0"
---
+++ src/hm_python/console.py
@@ -0,0 +1,30 @@
+# src/hm_python/console.py
+import textwrap
+
+import click
+import requests
+
+from . import __version__, wikipedia
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+
+@click.command()
+@click.option(
+        "--language",
+        "-l",
+        default="en",
+        help="Language edition of Wikipedia",
+        metavar="LANG",
+        show_default=True,
+)
+@click.version_option(version=__version__)
+def main():
+    """The hypermodern Python project"""
+
+    data = wikipedia.random_page(language=language)
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
